eb1d8a5f607ff4fd61690e13704ce5212724d4cb
claudiay <yadathi@gmail.com>
2013-06-27 02:13:43 -0700

    Wrote solutions and cleaned up comments for rotating matrix.

--- turn_matrix.py
+++ turn_matrix.py
@@ -1,59 +1,56 @@
-'''
-rotate the matrix
+#!/usr/bin/env python
+"""
+Given an NxN matrix, rotate the matrix by 90 degrees and return rotated matrix.
+Example:
+    input: 
+           [[1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]]
+    output:
+           [[13, 9, 5, 1],
+            [14, 10, 6, 2],
+            [15, 11, 7, 3],
+            [16, 12, 8, 4]]
+"""
 
 
-1 2 3
-4 5 6 
-7 8 9
+def rotate(matrix):
+    # Short without zip.
+    return [[j[i] for j in matrix][::-1] for i in range(len(matrix))]
 
-switch to 
 
-7 4 1
-8 5 2
-9 6 3
+def rotate_with_zip(matrix):
+    # Python trick answer, returns an array with tuples. 
+    return zip(*matrix[::-1])
 
-'''
 
-function rotateMatrix(matrix) {
-    for(row=0; row<=matrix.length(); row++){
-        for(col=0; col<=matrix.length; col++){
-            new matrix = [[],[],[]];
-            temp val = matrix[row][col]
-            new matrix[col]
-        }
-    }
-}
+def rotate_explain(matrix):
+    # A expanded version to explain each step.
+    n = len(matrix)
+    new_matrix = []
 
-# assume data structure is a list of coordinates for the numbers
+    for i in range(n):
+        new_row = []
+        for row in matrix:
+            row.append(row[i])
+        new_matrix.append(new_row[::-1]) # Reverse new_row before appending.
+    
+    return new_matrix
 
-def flip_matrix(mat):
-    # needed a midpoint lenght to compare to but could just set 1 or 2 depending on if coord start at 0,0 or 1,1
-    mid = int(len(matrix/6))
-    for i, coord in enumerate(matrix):
-        if i == 0:
-            pass
-        # if x is 1 then it just flips x & y
-        elif mid == coord[0]:
-            coord[0], coord[1] = coord[1], coord[0]
-        # if x is less than 1 then x becomes y and y becomes 2
-        elif mid > coord[1]:
-            coord[0] = coord[1]
-            coord[1] = mid + 1
-        # if x is greater than 1 then x becomes y and y becomes 1
-        elif mid < coord[0]:
-            coord[0] = coord[1]
-            coord[1] = mid - 1
 
-var: x1, x2, y1, y2
-newMatrix = []
-for(var i=1; i <= matrix.length(); i++){
-    newMatrix.push[()];
-}
-for(y1=0, y2=matrix.length-1; y1<= matrix.length;, y2>=; y1++, y2--){
-    for(x1=0, x2=matrix[y1].length; x<=matrix[y1].length), x2>=0; x1++, x2--){
-    newMatrix[x1][y2] = matrix[y1][x1];
-    }
-    return newMatrix;
-}
+def rotate_in_place(matrix):
+    # TODO rotate the matrix in place, without creating another matrix.
+    return False
 
-# better way new[x,y] = old [y,2-x]
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
+    
+    print rotate(matrix)
+    print rotate_with_zip(matrix)
+    print rotate_explain(matrix)
+    print rotate_in_place(matrix)
