59d4753fa1bf825fe8ba41607146fb3f87e19402
nyghtowl <warrick.melanie@gmail.com>
2013-06-30 22:49:55 -0700

    Added challenge to expand the problemset. Still to be coded. Additionally need to rework tests.

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@
 Retun true if a palindrome
 '''
 
-#palandromes / symmetry
+#Recursive solution
 def pal(word):
     if len(word) < 2:
         return True
@@ -11,10 +11,17 @@ def pal(word):
     if word[0] != word [-1]:
         return False
 
-print pal('rowme')
-
+#Simple reverse list solution
 def pal2(word):
     return word == word[::-1]
+
+#Solve by skipping over spaces and punctuation
+def pal3(word):
+    pass
+    '''
+    kept a cursor on the left (start from 0) and a cursor on the right (start from len(string)-1) (increment and decrease the left & right indices respectively if not valid), check if they are equal, and basically do it until the two cursors have crossed over (where right < left)> 
+    '''
+
 
 print pal2('rowme')
 
