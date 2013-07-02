dbefe5f53636c7b7ad60634c43d7a31b6cd2946a
nyghtowl <warrick.melanie@gmail.com>
2013-07-01 21:57:01 -0700

    Add time complexity to notes and added additional approach to solve with creating a hash and then a list based on what is in the hash.

--- list_intersection.py
+++ list_intersection.py
@@ -2,7 +2,7 @@
 Find list intersection
 '''
 
-#Return list of intersections between 2 lists - O(n^2)
+#Return list of intersections between 2 lists - O(n^2) or O(nm)
 def list_intersection(list1, list2):
     result = []
     for a in list1:
@@ -11,17 +11,23 @@ def list_intersection(list1, list2):
                 result.append(a)
     return result
 
-#Return list of intersections between 2 lists - O(n)
+#Return list of intersections between 2 lists - O(n^2) or O(nm)
 def list_intersection2(list1, list2):
     result = []
-    for a in list1:
-        if a in list2:
+    for a in list1: 
+        if a in list2: #'in' is a loop
             result.append(a)
     return result
 
-#Return list of intersections using list and set functions
+#Return list of intersections using list and set functions - O(n)
 def list_intersection3(list1, list2):
     return list(set(list1) & set(list2))
+
+#O(n)
+def list_intersection4(list1, list2):
+    s1 = set(list1)
+    lc = [list2[i] for i in xrange(len(list2)) if list2[i] in s1]
+    return list(lc)
 
 #Test for find number variations 
 implementations = [list_intersection, list_intersection2, list_intersection3]
