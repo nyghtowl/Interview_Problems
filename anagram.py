d7e9150fcd8f35eed7066805ab95b1c32b7cc3d5
nyghtowl <warrick.melanie@gmail.com>
2013-07-03 09:10:51 -0700

    Added variation that allows for returning dict values. Added tests and comments.

--- anagram.py
+++ anagram.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 """
-Give a list of strings, return another list of anagram sets.
+Anagram 
+(A word, phrase, or name formed by rearranging the letters of anothe)
+
+Give a list of strings, return a list of anagram sets just from the original input.
+
 Example:
     Input: ['cat', 'tablet', 'wolf', 'act', 'battle', 'flow', 'batlet', 'food']
     Output: [['cat', 'act'], ['tablet', 'battle', 'batlet'], ['wolf', 'flow']]
+
+Note: It does not generate any new words that are not in the input
 """
 
 def anagram(words):
@@ -19,6 +25,7 @@ def anagram(words):
         else:
             matches[key] = [word]
 
+    #Creates result list of lists 
     for k, v in matches.items():
         if len(v) >= 2:
             results.append(v)
@@ -26,7 +33,32 @@ def anagram(words):
     return results
 
 
+def anagram2(words):
+    matches = {}
+
+    # Create a dict with sorted values as key, and list of original strings as
+    # the value.
+    for word in words:
+        key = ''.join(sorted(list(word)))
+        if matches.get(key, False):
+            matches[key].append(word)
+        else:
+            matches[key] = [word]
+
+    #Pops value from dict if not more than 1 and returns dictionary values
+    for k, v in matches.items():
+        if len(v) <= 1:
+            matches.pop(k, None)
+
+    return matches.values()
+
 if __name__ == '__main__':
+
+    words_input = ['cat', 'tablet', 'wolf', 'act', 'battle', 'flow', 'batlet', 'food']
+    anagram_result = ['tablet', 'battle', 'batlet'], ['wolf', 'flow'], ['cat', 'act']
+
+
     print anagram(['cat', 'tablet', 'wolf', 'act', 'battle', 'flow', 'batlet',
                     'food'])
-
+    print anagram2(['cat', 'tablet', 'wolf', 'act', 'battle', 'flow', 'batlet',
+                    'food'])
