54ffafb6b9153e391a5faeaee1dea41b4c255881
jofusa <dennison.john@gmail.com>
2013-07-03 09:12:10 -0600

    added old example back in

--- palindrome.py
+++ palindrome.py
@@ -22,6 +22,14 @@ def pal3(word):
     kept a cursor on the left (start from 0) and a cursor on the right (start from len(string)-1) (increment and decrease the left & right indices respectively if not valid), check if they are equal, and basically do it until the two cursors have crossed over (where right < left)> 
     '''
 
+def pal4(word):
+    """ function to strip out space and uppercases """
+    stripped_word = word.replace(' ','').lower()
+    return stripped_word == stripped_word[::-1]
+
 
 print pal2('rowme')
 
+print pal4('rowme')
+print pal4('Lion oil')
+print pal4('a man a plan a canal Panama')
