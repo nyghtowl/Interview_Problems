5e959a933305ffa92098fcdc7869d458a79ef295
nyghtowl <warrick.melanie@gmail.com>
2013-07-03 00:18:39 -0700

    Added tests for breadth first traversal and extended tree structure. Breadth first search example still needed. Also None is printing between example 2 and 3 which needs to be cleaned.

--- bfs.py
+++ bfs.py
@@ -1,5 +1,7 @@
 '''
-Breadth First Search
+Breadth First Search & Breadth First Traversal
+
+Output: printing out results of the tree
 '''
 
 
@@ -13,24 +15,27 @@ class Node():
     #Prints each value moving across each layer
     def bft(self):
         q = []
+        ans = []
         q.append(self)
         while q:
             n = q.pop(0)
-            print n.val
+            ans.append(n.val)
             if n.left:
                 q.append(n.left)
             if n.right:
                 q.append(n.right)
+        return ' '.join(ans)
 
 
     #Prints each level on a separate line.
     def bft2(self):
         q = []
         q2 = []
+        ans = []
         q.append(self)
         while q:
             n = q.pop(0)
-            print n.val,
+            ans.append(n.val)
             if n.left:
                 q2.append(n.left)
             if n.right:
@@ -38,35 +43,50 @@ class Node():
             if len(q) == 0:
                 q = q2
                 q2 =[]
-                print ""
+                ans.append('\n')
+        return ' '.join(ans)
 
 
     #Variation that prints each level on a separate line.
     def bft3(self):
         parent = [self]
+        ans = []
         while parent:
             children = []
             for i, node in enumerate(parent):
-                print node.val,
+                ans.append(node.val)
                 if node.left:
                     children.append(node.left)
                 if node.right:
                     children.append(node.right) 
             parent = children
-            print ""
+            print ans.append('\n')
+        return ' '.join(ans)
 
 
-#Build out the tree.
-n = Node('Lola')
-n2 = Node('Ann')
-n3 = Node('Rose')
-n4 = Node('Janice')
-n5 = Node('Harriet')
-n6 = Node('Louis')
-n.left = n2
-n.right = n3
-n.right.left = n4
-n.right.right = n5
-n.left.left = n6
 
-n.bft3()
+#Run, build & test the tree
+if __name__ == '__main__':
+    #Build out the tree.
+    n = Node('Lola')
+    n2 = Node('Ann')
+    n3 = Node('Rose')
+    n4 = Node('Janice')
+    n5 = Node('Harriet')
+    n6 = Node('Louis')
+    n7 = Node('Gertrude')
+
+    n.left = n2
+    n.right = n3
+    n.right.left = n4
+    n.right.right = n5
+    n.left.left = n6
+    n.left.right = n7
+
+    result1 = 'Lola Ann Rose Louis Gertrude Janice Harriet'
+    result2 = 'Lola \n Ann Rose \n Louis Gertrude Janice Harriet'
+
+    print "n.bft() == %s : %s" % (result1, (n.bft() == result1))
+    print "n.bft2() == \n %s" % (n.bft2())
+    print "n.bft3() == \n %s" % (n.bft3())
+
