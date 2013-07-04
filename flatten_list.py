cd704ba2c6af9ffb0575ba6399cd00b93c41e4b8
nyghtowl <warrick.melanie@gmail.com>
2013-07-03 21:50:17 -0700

    Built out problem. Setup function struction and test.

--- flatten_list.py
+++ flatten_list.py
@@ -1,4 +1,18 @@
 '''
 Flatten lists
 '''
-[item for sublist in l for item in sublist]
+
+def flatten_list(l):
+    return [item for sublist in l for item in sublist]
+
+
+#Test
+l = [[1,2],[3,4],[5,6]]
+result = [1,2,3,4,5,6]
+
+implementations = [flatten_list]
+
+for impl in implementations:
+    print "trying %s" % impl
+    print "  f(l) == %s: %s" % (result, (impl(l) == result))
+
