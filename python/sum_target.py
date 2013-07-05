1af7eefa4772fb7e5050f5a4c10afa9dbcb1f0a5
nyghtowl <warrick.melanie@gmail.com>
2013-07-05 15:09:44 -0700

    Cleaned up tests and revised functions to meet tests.

--- python/sum_target.py
+++ python/sum_target.py
@@ -7,23 +7,40 @@ Challenge:
 *Given a list of numbers, return a set of 3 numbers that add to 0.
 '''
 
-def sum_target(target, list):
+# Embedded loops. - O(n)
+def sum_target(target, num_list):
     ans = False
-    for i in list:
-        for j in list:
+    for i in num_list:
+        for j in num_list:
             if (i+j) == target:
                 ans = True
     return ans
 
-alist = [5,3,5,7,1,2,5,6]
-print a, sum_target(20, alist)
-
-def sum_target2(target, list):
-    if list <= 1 and list[0] != target:
+#Recursive solution.
+def sum_target2(target, num_list):
+    if len(num_list) <= 1 and num_list[0] != target:
         return False
-    if (list[-2]+list[-1]) == target:
-        return True
+    elif num_list[-2]:
+        if (num_list[-2]+num_list[-1]) == target:
+            return True
+        else:
+            return sum_target2(target, num_list[:-1])
     else:
-        return sum_target2(target, list[:-1])
+        return False
 
-print 2, sum_target(8, alist)
+
+#Test section.
+num_list = [5,3,5,7,1,2,5,6]
+target = 20
+target2 = 8
+target3 = 0
+
+result = True
+result2 = False
+
+implementations = [sum_target, sum_target2]
+
+for impl in implementations:
+    print "f(%s) == []: %s" % (target, impl(target, num_list) == result2)
+    print "f(%s) == [5,3] or [7,1]: %s" % (target2, impl(target2, num_list) == result)
+    print "f(%s) == []: %s" % (target3, impl(target3, num_list) == result2)
