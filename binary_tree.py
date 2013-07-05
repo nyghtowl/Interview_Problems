7ea1e45c060e39fad47f43908de9bc3acd0784f9
nyghtowl <warrick.melanie@gmail.com>
2013-07-05 09:04:35 -0700

    Moved to pyhon folder.

--- binary_tree.py
+++
@@ -1,6 +0,0 @@
-'''
-Binary Search 
-
-Validate a binary search tree. I did it using space O(1) (though I also suggested at first you can pop it in order to an array, then check the array-- which is more intuitive to understand but not space or time efficient). 
-(I had a sentinel value that each node is checking against-- you can pass this into the function directly but I put it outside of the function for simplicity). Time efficiency for this is o(log2(n))
-'''
