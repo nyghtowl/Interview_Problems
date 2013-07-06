9cb62ed4105527858efda82604c304715fa0bd7a
nyghtowl <warrick.melanie@gmail.com>
2013-07-05 23:51:02 -0700

    Added a couple examples from HB today. Expanded tests. Psuedo Boyer example is still a work in progress. Need to resolve the issue with shifting past the length of the large string. Also need to account for setting end index when the matching letter exists in middle of string.

--- python/ransome.py
+++ python/ransome.py
@@ -1,5 +1,8 @@
 '''
 Find if a smaller string exists and/or can be created out of a larger string
+
+
+
 '''
 
 #Ransome note example find out if the letters are in the mag
@@ -10,7 +13,7 @@ def mag_dict(magazine):
     return mag_hash
 
 
-def comparison(note, mag):
+def comparison(mag, note):
     mdict = mag_dict(mag)
     for i in note:
         if i in mdict:
@@ -19,10 +22,108 @@ def comparison(note, mag):
             return False
     return True
 
-print 8, comparison('hash', 'this is a hash')
-
-
 
 def strstr(needle, haystack):
     pass
+
+
+#WIP
+def match(str1,str2):
+    l_str1 = list(str1)
+    l_str2 = list(str2)
+    state = True
+
+    #Check which string would be a subset.
+    if len(l_str1) > len(l_str2):
+        large_list, small_list = l_str1, l_str2
+    else:
+        large_list, small_list = l_str1, l_str2
+
+    for letter in large_list:
+        if letter not in small_list:
+            state = False
+
+# in progress
+
+    return state
+
+#Embedded loop solution. - O(n+m)
+def match2(s1,s2):
+    state = False
+    for i, char in enumerate(s1):
+        if s2[0] == char:
+            j=0
+            while j < len(s2):
+                if i+j < len(s1):
+                    if s2[j] != s1[i+j]:
+                        break
+                    else: 
+                        state = True
+                        j += 1
+                else:
+                    break
+    return state
+
+
+#Boyer Moore Algorithm approach - compare starting end of string 
+#check each subset of the main string whether the last character is
+#included. If not then shift out another subset. - O(n-m)
+
+def match3(s1,s2):
+    j_d = {}
+    for c in s1:
+        j_d[c] = True
+
+    i = len(s2)-1
+    while i < len(s1):
+        if not j_d.get(s1[i]):
+            if i >= len(s1):
+                i = len(s1)-1
+            else:
+                i += len(s2)
+        else:    
+            j = len(s2)-1            
+            while j >= 0: 
+                found = False
+                a = 0
+                print 1, s2[j]
+                print 2, s1[i]
+                print 3, j
+
+                if s2[j] != s1[i]:
+                    break
+                
+                j -= 1
+                i -= 1
+            
+            i += len(s2)-1
+
+        if j == -1:
+            found = True
+            break
+
+
+    return found
+
+#Test section.
+str1 = 'Cat goes postal'
+str1a = 'postal'
+str1b = 'Cat'
+
+str2 = 'this is a hash'
+str2a = 'hash'
+str2b = 'not'
+
+result = True
+result2 = False
+
+implementations = [comparison, match2, match3]
+
+for impl in implementations:
+    print "trying %s" % impl
+    print "f(%s) == %s: %s" % (str1a, result, (impl(str1, str1a) == result))
+    print "f(%s) == %s: %s" % (str1b, result, (impl(str1, str1b) == result))
+    print "f(%s) == %s: %s" % (str2a, result, (impl(str2, str2a) == result))
+    print "f(%s) == %s: %s" % (str2b, result2, (impl(str2, str2b) == result2))
+
 
