d96f2bd1558e8c5feee2e4361fffafb356e6109b
nyghtowl <warrick.melanie@gmail.com>
2013-07-08 16:23:22 -0700

    Added test structure to dfs.

--- python/dfs.py
+++ python/dfs.py
@@ -1,8 +1,8 @@
 '''
 Depth First Search
 '''
-class Node_DFS():
-    def __init__(self):
+class Node():
+    def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
@@ -12,16 +12,41 @@ class Node_DFS():
         if not n:
             return
         else:
-            hold = '%r' % n.val
+            hold = '%s ' % n.val
             if n.left:
-                hold += '%r' % dfs(n.left)    
+                hold += '%s' % n.left.dfs()    
             if n.right:
-                hold += '%r' % dfs(n.right)
+                hold += '%s' % n.right.dfs()
 
         return hold
 
-n = Node('Lola')
-n2 = Node('Ann')
-n3 = Node('Rose')
-n.left = n2
-n.right = n3
+if __name__ == '__main__':
+
+
+    #Build out the tree.
+    n = Node('Lola')
+    n2 = Node('Ann')
+    n3 = Node('Rose')
+    n4 = Node('Janice')
+    n5 = Node('Harriet')
+    n6 = Node('Louis')
+    n7 = Node('Gertrude')
+
+    n.left = n2
+    n.right = n3
+    n.right.left = n4
+    n.right.right = n5
+    n.left.left = n6
+    n.left.right = n7
+
+    #Test
+    implementations = [n.dfs]
+    
+    result = 'Lola Ann Louis Gertrude Rose Janice Harriet '
+
+    for impl in implementations:
+        print "trying %s" % impl
+        print " f(tree) == %s: %s" % (result, impl() == result)
+
+
+
