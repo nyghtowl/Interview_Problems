6c91f506770f34d36ef6c48ead79663b708e8b3d
nyghtowl <warrick.melanie@gmail.com>
2013-07-14 08:21:34 -0700

    Aligned structure to format.

--- python/equal_prob.py
+++ python/equal_prob.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
 '''
 Equal Probability
-Shuffle a string to make sure that it returns an equal-probability. Basically, for string of length n, you have n! permutations-- so the string you return should reflect 1/n!. What I did was pick a random number between (i, n-1) and increment i until it reaches len(string)-1, then take that random index and swap it with the first character of the string. As you iterate, you only look at the slice of string (i: len(string)-1) because the swapped characters (ones in the front) have been used already, and shouldn't be touched again.
+
+Input: string
+Output: Shuffle the string to make sure that it returns an equal-probability
+
+Example: For string of length n, you have n! permutations-- so the string you return should reflect 1/n! 
+
+What I did was pick a random number between (i, n-1) and increment i until it reaches len(string)-1, then take that random index and swap it with the first character of the string. 
+
+As you iterate, you only look at the slice of string (i: len(string)-1) because the swapped characters (ones in the front) have been used already, and shouldn't be touched again.
 '''
