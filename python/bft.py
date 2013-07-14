eb13bd1d978bdd3ebe5ee823c931a145fb87a137
nyghtowl <warrick.melanie@gmail.com>
2013-07-13 21:53:26 -0700

    Applied pep 8 and updated comments.

--- python/bft.py
+++ python/bft.py
@@ -1,18 +1,20 @@
+#!/usr/bin/env python
 '''
 Breadth First Traversal
 
-Input: tree
+Input: binary tree
 Output: printing out results of the tree by traversing the tree.
 '''
 
 
 class Node():
+
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
 
-    #Prints each value moving across each layer. O(n)
+    # Prints each value moving across each layer. O(n)
     def bft(self):
         q = []
         ans = []
@@ -26,8 +28,7 @@ class Node():
                 q.append(n.right)
         return ' '.join(ans)
 
-
-    #Prints each level on a separate line. O(n)
+    # Prints each level on a separate line. O(n)
     def bft2(self):
         q = []
         q2 = []
@@ -42,12 +43,11 @@ class Node():
                 q2.append(n.right)
             if len(q) == 0:
                 q = q2
-                q2 =[]
+                q2 = []
                 ans.append('\n')
         return ' '.join(ans)
 
-
-    #Variation that prints one line breadth. O(n)
+    # Variation that prints one line breadth. O(n)
     def bft3(self):
         parent = [self]
         ans = []
@@ -58,15 +58,14 @@ class Node():
                 if node.left:
                     children.append(node.left)
                 if node.right:
-                    children.append(node.right) 
+                    children.append(node.right)
             parent = children
         return ' '.join(ans)
 
 
-
-#Run, build & test the tree.
+# Run, build & test the tree.
 if __name__ == '__main__':
-    #Build out the tree.
+    # Build out the tree.
     n = Node('Lola')
     n2 = Node('Ann')
     n3 = Node('Rose')
@@ -82,9 +81,8 @@ if __name__ == '__main__':
     n.left.left = n6
     n.left.right = n7
 
-
-    #Test section. Split implementations since results are different.
-    implementations = [n.bft,n.bft3]
+    # Test section. Split implementations since results are different.
+    implementations = [n.bft, n.bft3]
     implementations2 = [n.bft2]
 
     result1 = 'Lola Ann Rose Louis Gertrude Janice Harriet'
@@ -93,7 +91,6 @@ if __name__ == '__main__':
     for impl in implementations:
         print "trying %s" % impl
         print " f(tree) == %s: %s" % (result1, impl() == result1)
-
 
     for impl in implementations2:
         print "trying %s" % impl
