55b472b26b1b9fed10f42b80f30e670a84d90296
nyghtowl <warrick.melanie@gmail.com>
2013-07-14 07:41:26 -0700

    Reformatted structure, revised problem structure to align to dictionary syntax. Added tests. Changed variables for clarity. Error still throwing on line 18. Work in progress.

--- python/itinerary.py
+++ python/itinerary.py
@@ -1,34 +1,49 @@
-
+#!/usr/bin/env python
 '''
-print out the cities for itinerary in order
+Travel Itinerary
+
+Input: city_pairs of of paired travel cityations (from to)
+Output: Print out the cities in order based on travel plan
 
 
-SFO - LAX
-LAX - BOS
-JFK - SFO
-BOS - DEN
+'SFO' - 'LAX'
+'LAX' - 'BOS'
+'JFK' - 'SFO'
+'BOS' - 'DEN'
 '''
 
-def orig(list):
-    org_dict = {}
-    for i, loc in enumerate(list):
-        if loc[0] in orig_dict:
-            orig_dict[loc[0]].append(0)
+def sort_cities(city_pairs):
+    visit_count = {}
+    for index, city in enumerate(city_pairs):
+        if visit_count[city[0]]:
+            visit_count[city[0]] += 1
         else:
-            orig_dict[loc[0]] = [0]
-        if loc[1] in orig_dict:
-            orig_dict[loc[1]].append(1)
+            visit_count[city[0]] = 1
+        if visit_count[city[1]]:
+            visit_count[city[1]] += 1
         else:
-            orig_dict[loc[1]] = [1]
-    return iten(orig_dict, list)
+            visit_count[city[1]] = 1
+    return print_cities(visit_count, city_pairs)
 
-def iten(dict, list):
-    for key, value in dict:
-        if value == [0]:
-            print key
-            orig = key
-            for i, loc in enumerate(list):
-                if orig == loc[0]:
-                    print loc[1]
-                    orig = loc[1]
-                    list.pop(i)
+def print_cities(visit_count, city_pairs):
+    for city, visit in visit_count:
+        if visit == [0]:
+            print city
+            orig = city
+            for index, city in enumerate(city_pairs):
+                if orig == city[0]:
+                    print city[1]
+                    orig = city[1]
+                    city_pairs.pop(index)
+
+# Test section
+
+# City pairs format is (from, to)
+city_pairs = [('SFO','LAX'),('BOS','DEN'),('LAX','BOS'),('JFK','SFO')]
+result = [('JFK','SFO'),('SFO','LAX'),('LAX','BOS'),('BOS','DEN')]
+
+implementations = [sort_cities]
+
+for impl in implementations:
+    print 'for %s in implementations' % impl
+    print 'f(%s) == %s: %s' % (city_pairs, result, impl(city_pairs) == result)
