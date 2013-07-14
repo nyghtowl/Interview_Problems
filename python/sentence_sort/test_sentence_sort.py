3c1f4c4042678d4dfa1949af9e1d8c2425111a6a
nyghtowl <warrick.melanie@gmail.com>
2013-07-14 08:23:57 -0700

    Aligned structure to format.

--- python/sentence_sort/test_sentence_sort.py
+++ python/sentence_sort/test_sentence_sort.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
 '''
+Sentence Sort Test
+
+Test file for sentence_sort problem
+
 Original problem/solution submission from jofusa
 '''
 
 from sentence_sort import *
-
-
 
 def test_sentence_sort():
     test = "This is the First"
