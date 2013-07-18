d1b1170b168dcf64e4c0f453c4a5d6d55b460402
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 23:23:44 -0700

    Setup file so it can be imported.

--- python/flatten_list.py
+++ python/flatten_list.py
@@ -10,14 +10,14 @@ Output: contcatenation of lists into one
 def flatten_list(list_of_lists):
     return [item for sublist in list_of_lists for item in sublist]
 
+if __name__ == '__main__':
+    #Test section
+    list_of_lists = [[1,2],[3,4],[5,6]]
+    result = [1,2,3,4,5,6]
 
-#Test section
-list_of_lists = [[1,2],[3,4],[5,6]]
-result = [1,2,3,4,5,6]
+    implementations = [flatten_list]
 
-implementations = [flatten_list]
-
-for impl in implementations:
-    print "trying %s" % impl
-    print "  f(%s) == %s: %s" % (list_of_lists, result, (impl(l) == result))
+    for impl in implementations:
+        print "trying %s" % impl
+        print "  f(%s) == %s: %s" % (list_of_lists, result, (impl(l) == result))
 
