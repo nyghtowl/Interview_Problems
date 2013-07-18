6d9dbd7cf9038ba15778e8ff33b0abbe1cc72b64
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 23:21:24 -0700

    Setup file so it can be imported.

--- python/prime.py
+++ python/prime.py
@@ -17,12 +17,13 @@ def find_primes(n):
         possible_divisor += 1
     return list(primes)
 
-#Test section
-impl = find_primes
-sample_n = [1,6,7,100]
-results = [[],[2,3],[7],[2,5]]
-for index, n in enumerate(sample_n):
-    # print "trying %s" % impl
-    print "f(%s) == %s: %s" % (n,results[index],(impl(n) == results[index]))
+if __name__ == '__main__':
+    #Test section
+    impl = find_primes
+    sample_n = [1,6,7,100]
+    results = [[],[2,3],[7],[2,5]]
+    for index, n in enumerate(sample_n):
+        # print "trying %s" % impl
+        print "f(%s) == %s: %s" % (n,results[index],(impl(n) == results[index]))
 
 
