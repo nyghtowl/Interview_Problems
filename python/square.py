b18ea73aa5807b0a76dc5d649e4ac816102d1a90
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 23:17:34 -0700

    Setup file so it can be imported.

--- python/square.py
+++ python/square.py
@@ -14,13 +14,14 @@ def square(num):
 def square2(num):
     return sum([num for n in range(num)])
 
-#Test section
-implementations = [square, square2]
+if __name__ == '__main__':
+    #Test section
+    implementations = [square, square2]
 
-for impl in implementations:
-    print "trying %s" % impl
-    print "  f(0) == 0: %s" % (impl(0) == 0)
-    print "  f(1) == 1: %s" % (impl(1) == 1)
-    print "  f(2) == 4: %s" % (impl(2) == 4)
-    print "  f(3) == 9: %s" % (impl(3) == 9)
-    print "  f(10) == 100: %s" % (impl(10) == 100)
+    for impl in implementations:
+        print "trying %s" % impl
+        print "  f(0) == 0: %s" % (impl(0) == 0)
+        print "  f(1) == 1: %s" % (impl(1) == 1)
+        print "  f(2) == 4: %s" % (impl(2) == 4)
+        print "  f(3) == 9: %s" % (impl(3) == 9)
+        print "  f(10) == 100: %s" % (impl(10) == 100)
