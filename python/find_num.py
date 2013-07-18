8a0aa15c2a054f6c7e7c1d00a9964fb3ce65dd3a
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 23:25:47 -0700

    Setup file so it can be imported.

--- python/find_num.py
+++ python/find_num.py
@@ -41,29 +41,29 @@ def miss_num(list1, list2):
 def miss_num2(list1, list2):
     return abs(sum(list1)-sum(list2))
 
+if __name__ == '__main__':
+    #Test section
+    implementations = [find_num, find_num2]
+    list1 = [5,4,1,2,8,3,-1,3,1]
 
-#Test section
-implementations = [find_num, find_num2]
-list1 = [5,4,1,2,8,3,-1,3,1]
+    result1 = True
+    result2 = False
+    result3 = "Doesn't exist"
 
-result1 = True
-result2 = False
-result3 = "Doesn't exist"
+    for impl in implementations:
+        print "trying %s" % impl
+        print "  f(%s, 0) == False or Doesn't exist: %s" % (list1, (impl(list1, 0) == result2 or impl(list1, 0) == result3))
+        print "  f(%s, 1) == True or 8: %s" % (impl(list1, 1) == (list1, result1 or impl(list1, 1) == 8))
+        print "  f(%s, 2) == True or Doesn't exist: %s" % (list1, (impl(list1, 2) == True or impl(list1, 2) == result3))
+        print "  f(%s, 3) == True or 7: %s" % (list1, (impl(list1, 3) == result1 or impl(list1, 3) == 7))
 
-for impl in implementations:
-    print "trying %s" % impl
-    print "  f(%s, 0) == False or Doesn't exist: %s" % (list1, (impl(list1, 0) == result2 or impl(list1, 0) == result3))
-    print "  f(%s, 1) == True or 8: %s" % (impl(list1, 1) == (list1, result1 or impl(list1, 1) == 8))
-    print "  f(%s, 2) == True or Doesn't exist: %s" % (list1, (impl(list1, 2) == True or impl(list1, 2) == result3))
-    print "  f(%s, 3) == True or 7: %s" % (list1, (impl(list1, 3) == result1 or impl(list1, 3) == 7))
+    #Test for find miss number variations 
+    implementations2 = [miss_num, miss_num2]
+    list2 = [5,4,7,2,1,8,3]
+    list3 = [5,4,7,2,8,3]
+    list4 = [5,4,7,2,1,8,3]
 
-#Test for find miss number variations 
-implementations2 = [miss_num, miss_num2]
-list2 = [5,4,7,2,1,8,3]
-list3 = [5,4,7,2,8,3]
-list4 = [5,4,7,2,1,8,3]
-
-for impl in implementations2:
-    print "trying %s" % impl
-    print "  f(%s, %s) == 1: %s" % (list2, list3, (impl(list2, list3) == 1))
-    print "  f(%s, %s) == 0: %s" % (list3, list4, (impl(list3, list4) == 0))
+    for impl in implementations2:
+        print "trying %s" % impl
+        print "  f(%s, %s) == 1: %s" % (list2, list3, (impl(list2, list3) == 1))
+        print "  f(%s, %s) == 0: %s" % (list3, list4, (impl(list3, list4) == 0))
