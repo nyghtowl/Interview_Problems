ba50133ca8e6d49b69cb87d2f964ff0058721b11
nyghtowl <warrick.melanie@gmail.com>
2013-07-19 17:39:24 -0700

    Added code.

--- python/seat_circle.py
+++ python/seat_circle.py
@@ -10,3 +10,16 @@ Pattern continues of skipping one and asking one to leave till one left
 Output: Return who the last person left
 '''
 
+def last_person(seats):
+    pop_odd = True
+    while len(seats) > 1:
+        for i, seat in enumerate(seats):
+            if pop_odd and i % 2 == 1:
+                del seats[i]
+            elif not pop_odd and i % 2 == 0:
+                del seats[i]
+
+
+#Test section
+
+seats = [1...100]
