aca843b2fd3686ada6fc9a2c0a7840ed3b827bd7
nyghtowl <warrick.melanie@gmail.com>
2013-07-19 17:30:58 -0700

    Add problem.

---
+++ python/linked_list_circle.py
@@ -0,0 +1,10 @@
+'''
+Linked List - Circle
+
+Input: 100 chairs in a circle
+Condition: 
+Pattern of change is that seat 1 is asked to stay, seat 2 is asked to leave, seat 3 is asked to stay...
+Pattern continues until the end
+
+Output: Return who the last person is to leave
+'''
