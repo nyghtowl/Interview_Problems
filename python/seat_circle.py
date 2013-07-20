beef51fc61508c981a6a7e130025faba83704938
nyghtowl <warrick.melanie@gmail.com>
2013-07-19 17:35:54 -0700

    Renamed file because not a linked list.

---
+++ python/seat_circle.py
@@ -0,0 +1,12 @@
+'''
+Seat Circle
+
+Input: 100 chairs in a circle and they are labeled from 1 to 100
+
+Condition: 
+Pattern of change is that seat 1 is asked to leave, seat 2 is asked to stay, seat 3 is asked to leave...
+Pattern continues of skipping one and asking one to leave till one left
+
+Output: Return who the last person left
+'''
+
