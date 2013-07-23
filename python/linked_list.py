f8e8fb95d9d642770506a66f85505fc682fdb424
nyghtowl <warrick.melanie@gmail.com>
2013-07-22 23:15:04 -0700

    Added beginning draft for linked lists.

---
+++ python/linked_list.py
@@ -0,0 +1,18 @@
+'''
+Linked List
+(Singuly vs. Doubly)
+
+Input: Empty
+Output: Define a linked list and be able to navigate, add and delete
+
+Node
+  data   next       data  next        data   next 
+[      |      ] -> [     |     ] -> [      |      ]
+
+head.next 
+n = head
+n = n.next 
+
+
+'''
+
