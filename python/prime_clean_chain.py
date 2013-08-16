997bdc5ed99f6230e51a7acdec58bed2464ed26a
nyghtowl <warrick.melanie@gmail.com>
2013-08-16 15:09:24 -0700

    Adding problem from class.

---
+++ python/prime_clean_chain.py
@@ -0,0 +1,17 @@
+'''
+Prime - 4th Clean Chain
+
+Input: Take a sequence of N numbers. We'll call the sequence a "Clean Chain of length N" if the sum of the first N - 1 numbers is evenly divisibly by the Nth number.
+
+For example, the sequence [2, 4, 6, 8, 10] forms a clean chain of length 5, since 2 + 4 + 6 + 8 = 20, which is divisible by 10, and the sequence has 5 numbers.
+The first clean chain formed out of the sequence of primes is simply [2], with length 1.
+The second is [2, 3, 5] with length 3.
+The third is [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71], with length 20
+
+Output: What is the length of the fourth clean chain formed out of only primes?
+
+Bonus: major bonus points, find the length of the fifth chain, too, and include it in the comments.
+
+Contribution: Jess
+
+'''
