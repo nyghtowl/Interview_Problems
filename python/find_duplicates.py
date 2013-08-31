d46de6af9ab0b65e9351c74bbbf869e7b70d0fe3
nyghtowl <warrick.melanie@gmail.com>
2013-08-30 22:22:01 -0500

    Added additional solution that improves space complexity by creating only one new variable vs. 2.

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -6,6 +6,7 @@ Input: list of Size N, integers from 1 to n
 Output: print out if there are duplicates
 '''
 
+#O(n)
 def find_dup(list1):
     count = {}
     ans = []
@@ -17,9 +18,20 @@ def find_dup(list1):
             ans.append(item) 
     return ans
 
+#O(n)
+def find_dup2(list1):
+    ans = []
+    for i,l in enumerate(list1):
+        if i < len(list1):
+            if l in list1[i+1:]:
+                ans.append(l) 
+    ans.sort()
+    return ans
+
+
 if __name__ == '__main__':
     #Test section
-    implementations = [find_dup]
+    implementations = [find_dup,find_dup2]
     input1 = [5,4,7,2,1,8,3,1,4]
     result1 = [1, 4]
 
