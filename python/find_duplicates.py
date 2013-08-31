e2aad7b7aaf20e2008e6186a41dd260d31a55eec
nyghtowl <warrick.melanie@gmail.com>
2013-08-30 22:13:52 -0500

    Added duplicates problem.

---
+++ python/find_duplicates.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+Find Duplicates
+
+Input: list of Size N, integers from 1 to n
+Output: print out if there are duplicates
+'''
+
+def find_dup(list1):
+    count = {}
+    ans = []
+    for l in list1:
+        count.setdefault(l,0)
+        count[l]+=1
+    for item, num in count.iteritems():
+        if num > 1:
+            ans.append(item) 
+    return ans
+
+if __name__ == '__main__':
+    #Test section
+    implementations = [find_dup]
+    input1 = [5,4,7,2,1,8,3,1,4]
+    result1 = [1, 4]
+
+    for impl in implementations:
+        print "  f(%s) == %s: %s" % (input1, result1, impl(input1) == result1)
