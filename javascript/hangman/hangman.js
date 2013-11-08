7eddaf560a335715cc83371267ecf19ae0b1cf72
nyghtowl <warrick.melanie@gmail.com>
2013-11-08 00:11:48 -0800

    Added comments and clarity on what's left to do with the game.

--- javascript/hangman/hangman.js
+++ javascript/hangman/hangman.js
@@ -1,5 +1,24 @@
 // Hangman Game
 
+/*
+    secretWord: string, the secret word to guess.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many 
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess 
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the 
+      partially guessed word so far, as well as letters that the 
+      user has not yet guessed.
+
+   TO DO - Add functionality to pull random word, tell the person if a letter was guessed and streamline code into smaller methods
+*/
 
 (function () {
  
