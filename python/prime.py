bdb895c2734c41bbc3860368c823bb85e1e956ed
nyghtowl <warrick.melanie@gmail.com>
2013-11-22 21:56:16 +0700

    Added variation structured with generator

--- python/prime.py
+++ python/prime.py
@@ -17,6 +17,22 @@ def find_primes(n):
         possible_divisor += 1
     return list(primes)
 
+'''
+Write a generator, genPrimes, that returns the sequence of prime numbers on successive calls to its next() method: 2, 3, 5, 7, 11, ...
+'''
+
+def genPrimes():
+    primes = []   # primes generated so far
+    last = 1      # last number tried
+    while True:
+        last += 1
+        for p in primes:
+            if last % p == 0:
+                break
+        else:
+            primes.append(last)
+            yield last
+
 if __name__ == '__main__':
     #Test section
     impl = find_primes
