63cd6d7611b3debaf940f13c5fe6ba3aaa6f0155
nyghtowl <warrick.melanie@gmail.com>
2013-12-15 15:13:52 -0800

    Reverse a word in javascript.

---
+++ javascript/reverse.js
@@ -0,0 +1,19 @@
+'''
+Reverse 
+
+Input: string, sentence or link
+
+Out: reverse of what is submitted
+
+'''
+
+var reverse = function(orig){
+	var result = [];
+	for (var i = orig.length; i >= 0 ;i--){
+		result.push(orig[i]);
+	}
+	console.log(result.join(""));
+}
+
+
+reverse("Hello");
