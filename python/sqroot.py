930c177c12f139510dcf4ee072fd2a67029da327
nyghtowl <warrick.melanie@gmail.com>
2014-01-01 15:36:07 -0800

    Updated structure and added tests.

--- python/sqroot.py
+++ python/sqroot.py
@@ -1,8 +1,11 @@
-# assume positive n
-# Newton's method is a popular solution for square root, but not implemented here.
-
 '''
 Find the square root of n.
+
+    Input: A number
+    Output: The square root or the integers closest to the square root
+    Assume: positive n
+
+ Newton's method is a popular solution for square root, but not implemented here.
 '''
 
 def sqrt(n):
@@ -13,15 +16,19 @@ def sqrt(n):
             if n < number * number:
                 return number, number - 1
 
-'''
-Helper function to use in sqrt function to calculate number squared
-'''
+
 def isSqrt(a,b):
+    '''
+    Helper function to use in sqrt function to calculate number squared
+    '''
     if a * a == b:
         return True
     else:
         return False
 
-print sqrt(30)
 
+# Test Section
+if __name__ == '__main__':
+    print "sqrt(25) = 5: %s" % (sqrt(25) == 5)
+    print "sqrt(30) = (6, 5): %s" % (sqrt(30) == (6,5))
 
