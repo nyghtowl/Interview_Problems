5325cf388f33573dbe8681545335751602cf0d32
nyghtowl <warrick.melanie@gmail.com>
2014-08-10 09:36:08 -0700

    Pulled leftover code from dev.

--- java/javacode/FizzBuzz.java
+++ java/javacode/FizzBuzz.java
@@ -18,13 +18,10 @@ public class FizzBuzz
 
         int num;
 
-        boolean fizzed;
-
         System.out.print("Enter a number for printing fizzbuzz:");
         num = keyboard.nextInt();
 
         for (int i = 1; i <= num; i++) {
-            // System.out.println(i);
 
             if (i % 3 == 0 && i % 5 == 0)
                 System.out.println("fizz buzz");
